76e6f4de118679d55ecb8129d9e06eee735ab200
Laura Ramos Souza <lauraramoss2022@gmail.com>
2024-08-21 17:47:19 +0000

    cll

---
+++ Location/app/(tabs)/index.tsx
@@ -0,0 +1,17 @@
+import { View, Image, StyleSheet, Platform } from 'react-native';
+import Home from './telas/home'
+
+export default function HomeScreen() {
+  return (
+   <View style={styles.container}>
+    <Home/>
+       </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+  },
+});
